bacd41701cc86fdce79c0cf5ae5743dfbb5a6983
HeitorDeAgostinPin <142326372+HeitorDeAgostinPin@users.noreply.github.com>
2024-04-11 09:43:42 -0300

    Update Contato.cs gambiarras

--- Contato.cs
+++ Contato.cs
@@ -1,4 +1,4 @@
-﻿/*using System;
+/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +8,15 @@ namespace CalendarioDeEventos
 {//classe para inserir informações do responsavel do evento
     public class Contato:Evento
     {
+        public string Id { get; set; }
         public string Nome { get; set; }
-        public int Telefone { get; set; }
+        public string Telefone { get; set; }
         public string Email { get; set; }
+        public string IdDoContato { get; set; }
+        public List<string> EventosRelacionados { get; set; }
+
+        
+
+        
     }
 }
